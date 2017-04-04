1312c830d10ac54bc6dce4828e9b6415b7b6edc4
saralip <saralip@csu.fullerton.edu>
2017-04-03 21:05:08 -0700

    Currency Picker

---
+++ CurrencyConverter/PickerViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Sara Lipowsky on 4/3/17.
+//  Copyright © 2017 Sara Lipowsky. All rights reserved.
+//
+
+import UIKit
+
+class PickerViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var currencyPickerView: UIPickerView!
+    
+    var pickerData = ["Dollar", "Euro", "Yen"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //tying curr picker to the view
+        currencyPickerView.dataSource = self
+        currencyPickerView.delegate = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+}
+
